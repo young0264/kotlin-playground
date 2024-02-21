9b25f3cb4145cfe43fe4e917e61ce8ec7d6877a0
남의영 <ny2485@naver.com>
2024-02-21 23:31:18 +0900

    kotlin init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val worlName = "스코월드"
+    var myName = "스크류바"
+    var myAge = 15
+    var myJob = "마법사"
+
+    println("===============${worlName}에 오신것을 환영합니다===============")
+    println("정보요")
+    println("이름 : ${myName}")
+    println("나이 : ${myAge}")
+    println("직업 : ${myJob }")
+
+}
