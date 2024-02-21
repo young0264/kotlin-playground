96dc0229d816890e0baf1f2e9c33d216adf8a752
남의영 <ny2485@naver.com>
2024-02-22 01:14:48 +0900

    iterator exam

---
+++ src/main/kotlin/IteratorExam.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var i = 0
+    var startNum = 0
+    var endNum = 5
+
+
+    while (i < 3) {
+        println("${i}번째 while 인쇄")
+        i++
+    }
+
+    var infos = arrayOf("가", "나", "다", "라")
+    for (info in infos) {
+        println(info)
+    }
+
+    for (i in startNum until endNum) {
+        println(i)
+    }
+
+    for (i in startNum .. endNum) {
+        println(i)
+    }
+
+}
